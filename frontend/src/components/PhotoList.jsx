2a176cccb307fb4eceb76507342aca0bf716df49
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-10-05 16:26:14 -0400

    added and passed props

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -4,9 +4,10 @@ import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
 
 const PhotoList = (props) => {
+  const { favorite, toggleFavorite } = props;
 
   const photos = props.photos.map((photo) => {
-    return <PhotoListItem key={photo.id} location={photo.location} urls={photo.urls} user={photo.user}/>
+    return <PhotoListItem key={photo.id} location={photo.location} urls={photo.urls} user={photo.user} toggleFavorite={() => {toggleFavorite(photo.id)}} favorite={favorite[photo.id]}/>
   });
 
   return (
