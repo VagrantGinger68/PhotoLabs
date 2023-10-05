8348d3c4f575ebd159623b83b419cb1318f3e8a6
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-10-05 16:26:52 -0400

    conditionally render the FavBadge

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -5,11 +5,13 @@ import TopicList from './TopicList';
 import FavBadge from './FavBadge';
 
 const TopNavigation = (props) => {
+  const {topics, favorite} = props;
+
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={props.topics}/>
-      <FavBadge />
+      <TopicList topics={topics}/>
+      {Object.values(favorite).includes(true) ? <FavBadge favorite={favorite} isFavPhotoExist/> : <FavBadge />}
     </div>
   )
 }
