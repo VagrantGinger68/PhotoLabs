f8bd009445142ef81167305a370f718f084a6c98
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-10-05 16:27:17 -0400

    Made the favorite state globally accessible

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 import '../styles/HomeRoute.scss';
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
 
 const HomeRoute = (props) => {
+
+  const { photos, topics } = props;
+
+  const [favorite, setFavorite] = useState({});
+
+  const toggleFavorite = (photoId) => {
+    setFavorite((prev) => ({
+      ...prev,
+      [photoId]: !prev[photoId]
+    }))
+  };
+
   return (
     <div className="home-route">
-      <TopNavigation topics={props.topics}/>
-      <PhotoList photos={props.photos}/>
+      {Object.values(favorite).includes(true)}
+      <TopNavigation topics={topics} favorite={favorite}/>
+      <PhotoList photos={photos} toggleFavorite={toggleFavorite} favorite={favorite}/>
     </div>
   );
 };
