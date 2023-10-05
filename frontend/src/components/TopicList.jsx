5f13e86a946bc7ea14765d84540c77d18bea0318
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-10-05 09:49:08 -0400

    Built the TopicListItem component

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/TopicList.scss";
+import TopicListItem from "./TopicListItem";
 
 const sampleDataForTopicList = [
   {
@@ -20,10 +21,14 @@ const sampleDataForTopicList = [
   },
 ];
 
+const topics = sampleDataForTopicList.map((topic) => {
+  return <TopicListItem key={topic.id} title={topic.title} slug={topic.slug}/>
+})
+
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
+      {topics}
     </div>
   );
 };
