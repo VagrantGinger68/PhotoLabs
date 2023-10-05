f32bf0f543b8508737780f82f790137bd2b2e7fb
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-10-05 10:06:43 -0400

    removed sample topic data

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -3,29 +3,12 @@ import React from "react";
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+const TopicList = (props) => {
 
-const topics = sampleDataForTopicList.map((topic) => {
-  return <TopicListItem key={topic.id} title={topic.title} slug={topic.slug}/>
-})
-
-const TopicList = () => {
+  const topics = props.topics.map((topic) => {
+    return <TopicListItem key={topic.id} title={topic.title} slug={topic.slug}/>
+  })
+  
   return (
     <div className="top-nav-bar__topic-list">
       {topics}
