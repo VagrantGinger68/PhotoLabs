14d27a9acc77dabf2c2c2f38c4fb8c9311a84150
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-10-07 09:56:22 -0400

    Reformatted and added props

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -4,17 +4,26 @@ import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
 
 const PhotoList = (props) => {
-  const { favorite, toggleFavorite } = props;
+  const { favorite, toggleFavorite, openPhotoView } = props;
 
+  
   const photos = props.photos.map((photo) => {
-    return <PhotoListItem key={photo.id} location={photo.location} urls={photo.urls} user={photo.user} toggleFavorite={() => {toggleFavorite(photo.id)}} favorite={favorite[photo.id]}/>
+    return (
+      <PhotoListItem
+        key={photo.id}
+        location={photo.location}
+        urls={photo.urls}
+        user={photo.user}
+        toggleFavorite={() => {
+          toggleFavorite(photo.id);
+        }}
+        favorite={favorite[photo.id] === undefined ? false : favorite[photo.id]}
+        openPhotoView={() => openPhotoView(photo)}
+      />
+    );
   });
 
-  return (
-    <ul className="photo-list">
-      {photos}  
-    </ul>
-  );
+  return <ul className="photo-list">{photos}</ul>;
 };
 
 export default PhotoList;
