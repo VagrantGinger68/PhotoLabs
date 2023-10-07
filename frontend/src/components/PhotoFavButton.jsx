42989034f1c6d4c98d7f934fcb41ee6e299ea14f
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-10-07 09:56:06 -0400

    changed spacing

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -5,7 +5,6 @@ import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton(props) {
   const { toggleFavorite, favorite} = props;
-
   return (
     <div className="photo-list__fav-icon">
       <div onClick={toggleFavorite} className="photo-list__fav-icon-svg">
