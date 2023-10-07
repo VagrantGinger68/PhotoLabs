577504a1ba705240775cab70b512c525e376fe7c
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-10-07 09:55:50 -0400

    Added the PhotoDetailModal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 const App = () => {
+
+  const [viewPhoto, setViewPhoto] = useState();
+
+  const openPhotoView = (photo) => {
+    setViewPhoto({photo})
+  };
+
+  const closePhotoView = () => {
+    setViewPhoto()
+  }
+
+  const [favorite, setFavorite] = useState({});
+
+  const toggleFavorite = (photoId) => {
+    setFavorite((prev) => (
+      {
+      ...prev,
+      [photoId]: !prev[photoId]
+    }))
+  };
+
   return (
     <div className="App">
-      <HomeRoute photos={photos} topics={topics}/>
+      <HomeRoute favorite={favorite} toggleFavorite={toggleFavorite} photos={photos} topics={topics} openPhotoView={openPhotoView}/>
+      {viewPhoto && <PhotoDetailsModal closePhotoView={closePhotoView} viewPhoto={viewPhoto} favorite={favorite} toggleFavorite={toggleFavorite} />}
     </div>
   );
 };
