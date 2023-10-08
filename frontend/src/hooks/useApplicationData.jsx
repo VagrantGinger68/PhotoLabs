69317dddecddcf9316dc4fe0de23b15bc1eecefc
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-10-07 20:14:05 -0400

    Implemented useEffect

--- frontend/src/hooks/useApplicationData.jsx
+++ frontend/src/hooks/useApplicationData.jsx
@@ -1,14 +1,20 @@
-import { useReducer } from 'react';
-import useWow from './useWow';
+import { useReducer, useEffect } from "react";
+import useWow from "./useWow";
 
 const initialState = {
   favorite: {},
   photoSelected: null,
+  photoData: [],
+  topicData: [],
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'TOGGLE_FAVORITE':
+    case "SET_PHOTO_DATA":
+      return { ...state, photoData: action.payload };
+    case "SET_TOPIC_DATA":
+      return { ...state, topicData: action.payload };
+    case "TOGGLE_FAVORITE":
       return {
         ...state,
         favorite: {
@@ -16,39 +22,51 @@ const reducer = (state, action) => {
           [action.payload.photoId]: !state.favorite[action.payload.photoId],
         },
       };
-    case 'SET_PHOTO_SELECTED':
+    case "SET_PHOTO_SELECTED":
       return {
         ...state,
         photoSelected: action.payload.photo,
       };
-    case 'CLOSE_PHOTO_DETAILS_MODAL':
+    case "CLOSE_PHOTO_DETAILS_MODAL":
       return {
         ...state,
         photoSelected: null,
       };
-      default:
-        throw new Error(
-          `Tried to reduce with unsupported action type: ${action.type}`
-        );
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
   }
 };
 
 const useApplicationData = () => {
   const { sayWow } = useWow();
 
+  useEffect(() => {
+    fetch("/api/photos")
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: "SET_PHOTO_DATA", payload: data }))
+  }, []);
+
+  useEffect(() => {
+    fetch("/api/topics")
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: "SET_TOPIC_DATA", payload: data }))
+  }, []);
+
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const updateToFavPhotoIds = (photoId) => {
-    dispatch({ type: 'TOGGLE_FAVORITE', payload: { photoId } });
+    dispatch({ type: "TOGGLE_FAVORITE", payload: { photoId } });
   };
 
   const setPhotoSelected = (photo) => {
     sayWow();
-    dispatch( {type: 'SET_PHOTO_SELECTED', payload: { photo } });
+    dispatch({ type: "SET_PHOTO_SELECTED", payload: { photo } });
   };
 
   const onClosePhotoDetailsModal = () => {
-    dispatch({ type: 'CLOSE_PHOTO_DETAILS_MODAL' });
+    dispatch({ type: "CLOSE_PHOTO_DETAILS_MODAL" });
   };
 
   return {
