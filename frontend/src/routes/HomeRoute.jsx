616b561ef77d837adc9a7b4ab7632e455fea2ec4
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-10-08 21:20:06 -0400

    Added setTopicSelected

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -7,11 +7,11 @@ import PhotoList from 'components/PhotoList';
 
 const HomeRoute = (props) => {
 
-  const { photos, topics, openPhotoView, favorite, toggleFavorite } = props;
+  const { photos, topics, openPhotoView, favorite, toggleFavorite, setTopicSelected } = props;
 
   return (
     <div className="home-route">
-      <TopNavigation topics={topics} favorite={favorite}/>
+      <TopNavigation topics={topics} favorite={favorite} setTopicSelected={setTopicSelected}/>
       <PhotoList photos={photos} toggleFavorite={toggleFavorite} favorite={favorite} openPhotoView={openPhotoView}/>
     </div>
   );
